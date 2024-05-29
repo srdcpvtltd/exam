8c99329c8d79906f42eda5e9ca416f2aa657b7c5
Jyotiranjan8596 <sahoorinku63@gmail.com>
2024-05-29 11:00:08 +0530

    Refactored the response format for the GET /subject API to ensure proper structure and consistency.

--- app/Http/Controllers/API/Student/SubjectController.php
+++ app/Http/Controllers/API/Student/SubjectController.php
@@ -6,13 +6,28 @@ use App\Http\Controllers\Controller;
 use App\Models\Subject;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class SubjectController extends Controller
 {
-    public function get_subject(){
-        $subjects = Subject::where('course_id',Auth::user()->studentProfile->course_id)
-        ->where('semester_id',Auth::user()->studentProfile->semester_id)
-        ->get();
-        dd($subjects->toArray());
+    public function get_subject()
+    {
+        $subjects = Subject::where('course_id', Auth::user()->studentProfile->course_id)
+            ->where('semester_id', Auth::user()->studentProfile->semester_id)
+            ->with(['course', 'semester'])
+            ->get();
+        // dd($subjects->course);
+        foreach ($subjects as $subject) {
+
+            // Log::info();
+            $subject->course_name = $subject->course->title;
+            $subject->semester_name = $subject->semester->name;
+            $subject->unsetRelation('course');
+            $subject->unsetRelation('semester');
+        }
+
+        return response()->json([
+            'data' => $subjects
+        ]);
     }
 }
